51ee8d339999e61e2f18dab252ffef818ab082c3
TaoMaru <jackson_maria1@columbusstate.edu>
2023-03-20 17:20:25 -0400

    Add btnClear event & call click on form load event

--- frmCatering.vb
+++ frmCatering.vb
@@ -1,3 +1,16 @@
 ﻿Public Class frmCatering
+    Private Sub frmCatering_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'uses btnClear event to reset form
+        btnClear.PerformClick()
+    End Sub
 
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'resets txtPoints to blank, resets focus onto txtPoints
+        'resests rad selections
+        txtPoints.Clear()
+        txtPoints.Focus()
+        radFruit.Checked = True
+        radPrePay.Checked = True
+        lblTotalCost.Text = ""
+    End Sub
 End Class
