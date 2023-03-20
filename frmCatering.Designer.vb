f49bd4623e87902023e3154548d312839c3b30af
TaoMaru <jackson_maria1@columbusstate.edu>
2023-03-20 16:44:07 -0400

    Add calc & clear btns, rename all components, set form startup to center screen

--- frmCatering.Designer.vb
+++ frmCatering.Designer.vb
@@ -22,229 +22,254 @@ Partial Class frmCatering
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.RadioButton6 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton7 = New System.Windows.Forms.RadioButton()
-        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.GroupBox1.SuspendLayout()
-        Me.GroupBox2.SuspendLayout()
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.lblSubheading = New System.Windows.Forms.Label()
+        Me.lblLoyaltyPoints = New System.Windows.Forms.Label()
+        Me.txtPoints = New System.Windows.Forms.TextBox()
+        Me.picPlatter = New System.Windows.Forms.PictureBox()
+        Me.lblPayment = New System.Windows.Forms.Label()
+        Me.radFruit = New System.Windows.Forms.RadioButton()
+        Me.radDeliMeat = New System.Windows.Forms.RadioButton()
+        Me.radPita = New System.Windows.Forms.RadioButton()
+        Me.radGrill = New System.Windows.Forms.RadioButton()
+        Me.radVeggie = New System.Windows.Forms.RadioButton()
+        Me.grpPlatterOptions = New System.Windows.Forms.GroupBox()
+        Me.lblTotalCost = New System.Windows.Forms.Label()
+        Me.radPrePay = New System.Windows.Forms.RadioButton()
+        Me.radPickupPay = New System.Windows.Forms.RadioButton()
+        Me.grpPayOption = New System.Windows.Forms.GroupBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        CType(Me.picPlatter, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.grpPlatterOptions.SuspendLayout()
+        Me.grpPayOption.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Label1
+        'lblHeading
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(136, 34)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(51, 17)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Location = New System.Drawing.Point(136, 34)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(51, 17)
+        Me.lblHeading.TabIndex = 0
+        Me.lblHeading.Text = "Label1"
         '
-        'Label2
+        'lblSubheading
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(139, 81)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(51, 17)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Label2"
+        Me.lblSubheading.AutoSize = True
+        Me.lblSubheading.Location = New System.Drawing.Point(139, 81)
+        Me.lblSubheading.Name = "lblSubheading"
+        Me.lblSubheading.Size = New System.Drawing.Size(51, 17)
+        Me.lblSubheading.TabIndex = 1
+        Me.lblSubheading.Text = "Label2"
         '
-        'Label3
+        'lblLoyaltyPoints
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(457, 273)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(51, 17)
-        Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Label3"
+        Me.lblLoyaltyPoints.AutoSize = True
+        Me.lblLoyaltyPoints.Location = New System.Drawing.Point(457, 273)
+        Me.lblLoyaltyPoints.Name = "lblLoyaltyPoints"
+        Me.lblLoyaltyPoints.Size = New System.Drawing.Size(51, 17)
+        Me.lblLoyaltyPoints.TabIndex = 2
+        Me.lblLoyaltyPoints.Text = "Label3"
         '
-        'TextBox1
+        'txtPoints
         '
-        Me.TextBox1.Location = New System.Drawing.Point(664, 270)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
-        Me.TextBox1.TabIndex = 3
+        Me.txtPoints.Location = New System.Drawing.Point(664, 270)
+        Me.txtPoints.Name = "txtPoints"
+        Me.txtPoints.Size = New System.Drawing.Size(100, 22)
+        Me.txtPoints.TabIndex = 3
         '
-        'PictureBox1
+        'picPlatter
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(610, 55)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(100, 50)
-        Me.PictureBox1.TabIndex = 4
-        Me.PictureBox1.TabStop = False
+        Me.picPlatter.Location = New System.Drawing.Point(610, 55)
+        Me.picPlatter.Name = "picPlatter"
+        Me.picPlatter.Size = New System.Drawing.Size(100, 50)
+        Me.picPlatter.TabIndex = 4
+        Me.picPlatter.TabStop = False
         '
-        'Label4
+        'lblPayment
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(150, 529)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(51, 17)
-        Me.Label4.TabIndex = 5
-        Me.Label4.Text = "Label4"
+        Me.lblPayment.AutoSize = True
+        Me.lblPayment.Location = New System.Drawing.Point(150, 529)
+        Me.lblPayment.Name = "lblPayment"
+        Me.lblPayment.Size = New System.Drawing.Size(51, 17)
+        Me.lblPayment.TabIndex = 5
+        Me.lblPayment.Text = "Label4"
         '
-        'RadioButton1
+        'radFruit
         '
-        Me.RadioButton1.AutoSize = True
-        Me.RadioButton1.Location = New System.Drawing.Point(78, 45)
-        Me.RadioButton1.Name = "RadioButton1"
-        Me.RadioButton1.Size = New System.Drawing.Size(115, 21)
-        Me.RadioButton1.TabIndex = 6
-        Me.RadioButton1.TabStop = True
-        Me.RadioButton1.Text = "RadioButton1"
-        Me.RadioButton1.UseVisualStyleBackColor = True
+        Me.radFruit.AutoSize = True
+        Me.radFruit.Location = New System.Drawing.Point(78, 45)
+        Me.radFruit.Name = "radFruit"
+        Me.radFruit.Size = New System.Drawing.Size(115, 21)
+        Me.radFruit.TabIndex = 6
+        Me.radFruit.TabStop = True
+        Me.radFruit.Text = "RadioButton1"
+        Me.radFruit.UseVisualStyleBackColor = True
         '
-        'RadioButton2
+        'radDeliMeat
         '
-        Me.RadioButton2.AutoSize = True
-        Me.RadioButton2.Location = New System.Drawing.Point(78, 84)
-        Me.RadioButton2.Name = "RadioButton2"
-        Me.RadioButton2.Size = New System.Drawing.Size(115, 21)
-        Me.RadioButton2.TabIndex = 7
-        Me.RadioButton2.TabStop = True
-        Me.RadioButton2.Text = "RadioButton2"
-        Me.RadioButton2.UseVisualStyleBackColor = True
+        Me.radDeliMeat.AutoSize = True
+        Me.radDeliMeat.Location = New System.Drawing.Point(78, 84)
+        Me.radDeliMeat.Name = "radDeliMeat"
+        Me.radDeliMeat.Size = New System.Drawing.Size(115, 21)
+        Me.radDeliMeat.TabIndex = 7
+        Me.radDeliMeat.TabStop = True
+        Me.radDeliMeat.Text = "RadioButton2"
+        Me.radDeliMeat.UseVisualStyleBackColor = True
         '
-        'RadioButton3
+        'radPita
         '
-        Me.RadioButton3.AutoSize = True
-        Me.RadioButton3.Location = New System.Drawing.Point(78, 129)
-        Me.RadioButton3.Name = "RadioButton3"
-        Me.RadioButton3.Size = New System.Drawing.Size(115, 21)
-        Me.RadioButton3.TabIndex = 8
-        Me.RadioButton3.TabStop = True
-        Me.RadioButton3.Text = "RadioButton3"
-        Me.RadioButton3.UseVisualStyleBackColor = True
+        Me.radPita.AutoSize = True
+        Me.radPita.Location = New System.Drawing.Point(78, 129)
+        Me.radPita.Name = "radPita"
+        Me.radPita.Size = New System.Drawing.Size(115, 21)
+        Me.radPita.TabIndex = 8
+        Me.radPita.TabStop = True
+        Me.radPita.Text = "RadioButton3"
+        Me.radPita.UseVisualStyleBackColor = True
         '
-        'RadioButton4
+        'radGrill
         '
-        Me.RadioButton4.AutoSize = True
-        Me.RadioButton4.Location = New System.Drawing.Point(78, 170)
-        Me.RadioButton4.Name = "RadioButton4"
-        Me.RadioButton4.Size = New System.Drawing.Size(115, 21)
-        Me.RadioButton4.TabIndex = 9
-        Me.RadioButton4.TabStop = True
-        Me.RadioButton4.Text = "RadioButton4"
-        Me.RadioButton4.UseVisualStyleBackColor = True
+        Me.radGrill.AutoSize = True
+        Me.radGrill.Location = New System.Drawing.Point(78, 170)
+        Me.radGrill.Name = "radGrill"
+        Me.radGrill.Size = New System.Drawing.Size(115, 21)
+        Me.radGrill.TabIndex = 9
+        Me.radGrill.TabStop = True
+        Me.radGrill.Text = "RadioButton4"
+        Me.radGrill.UseVisualStyleBackColor = True
         '
-        'RadioButton5
+        'radVeggie
         '
-        Me.RadioButton5.AutoSize = True
-        Me.RadioButton5.Location = New System.Drawing.Point(78, 198)
-        Me.RadioButton5.Name = "RadioButton5"
-        Me.RadioButton5.Size = New System.Drawing.Size(115, 21)
-        Me.RadioButton5.TabIndex = 10
-        Me.RadioButton5.TabStop = True
-        Me.RadioButton5.Text = "RadioButton5"
-        Me.RadioButton5.UseVisualStyleBackColor = True
+        Me.radVeggie.AutoSize = True
+        Me.radVeggie.Location = New System.Drawing.Point(78, 198)
+        Me.radVeggie.Name = "radVeggie"
+        Me.radVeggie.Size = New System.Drawing.Size(115, 21)
+        Me.radVeggie.TabIndex = 10
+        Me.radVeggie.TabStop = True
+        Me.radVeggie.Text = "RadioButton5"
+        Me.radVeggie.UseVisualStyleBackColor = True
         '
-        'GroupBox1
+        'grpPlatterOptions
         '
-        Me.GroupBox1.Controls.Add(Me.RadioButton1)
-        Me.GroupBox1.Controls.Add(Me.RadioButton5)
-        Me.GroupBox1.Controls.Add(Me.RadioButton2)
-        Me.GroupBox1.Controls.Add(Me.RadioButton4)
-        Me.GroupBox1.Controls.Add(Me.RadioButton3)
-        Me.GroupBox1.Location = New System.Drawing.Point(45, 131)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(272, 253)
-        Me.GroupBox1.TabIndex = 11
-        Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "GroupBox1"
+        Me.grpPlatterOptions.Controls.Add(Me.radFruit)
+        Me.grpPlatterOptions.Controls.Add(Me.radVeggie)
+        Me.grpPlatterOptions.Controls.Add(Me.radDeliMeat)
+        Me.grpPlatterOptions.Controls.Add(Me.radGrill)
+        Me.grpPlatterOptions.Controls.Add(Me.radPita)
+        Me.grpPlatterOptions.Location = New System.Drawing.Point(45, 131)
+        Me.grpPlatterOptions.Name = "grpPlatterOptions"
+        Me.grpPlatterOptions.Size = New System.Drawing.Size(272, 253)
+        Me.grpPlatterOptions.TabIndex = 11
+        Me.grpPlatterOptions.TabStop = False
+        Me.grpPlatterOptions.Text = "GroupBox1"
         '
-        'Label5
+        'lblTotalCost
         '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(471, 529)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(51, 17)
-        Me.Label5.TabIndex = 12
-        Me.Label5.Text = "Label5"
+        Me.lblTotalCost.AutoSize = True
+        Me.lblTotalCost.Location = New System.Drawing.Point(471, 529)
+        Me.lblTotalCost.Name = "lblTotalCost"
+        Me.lblTotalCost.Size = New System.Drawing.Size(51, 17)
+        Me.lblTotalCost.TabIndex = 12
+        Me.lblTotalCost.Text = "Label5"
         '
-        'RadioButton6
+        'radPrePay
         '
-        Me.RadioButton6.AutoSize = True
-        Me.RadioButton6.Location = New System.Drawing.Point(37, 30)
-        Me.RadioButton6.Name = "RadioButton6"
-        Me.RadioButton6.Size = New System.Drawing.Size(115, 21)
-        Me.RadioButton6.TabIndex = 13
-        Me.RadioButton6.TabStop = True
-        Me.RadioButton6.Text = "RadioButton6"
-        Me.RadioButton6.UseVisualStyleBackColor = True
+        Me.radPrePay.AutoSize = True
+        Me.radPrePay.Location = New System.Drawing.Point(37, 30)
+        Me.radPrePay.Name = "radPrePay"
+        Me.radPrePay.Size = New System.Drawing.Size(115, 21)
+        Me.radPrePay.TabIndex = 13
+        Me.radPrePay.TabStop = True
+        Me.radPrePay.Text = "RadioButton6"
+        Me.radPrePay.UseVisualStyleBackColor = True
         '
-        'RadioButton7
+        'radPickupPay
         '
-        Me.RadioButton7.AutoSize = True
-        Me.RadioButton7.Location = New System.Drawing.Point(37, 57)
-        Me.RadioButton7.Name = "RadioButton7"
-        Me.RadioButton7.Size = New System.Drawing.Size(115, 21)
-        Me.RadioButton7.TabIndex = 14
-        Me.RadioButton7.TabStop = True
-        Me.RadioButton7.Text = "RadioButton7"
-        Me.RadioButton7.UseVisualStyleBackColor = True
+        Me.radPickupPay.AutoSize = True
+        Me.radPickupPay.Location = New System.Drawing.Point(37, 57)
+        Me.radPickupPay.Name = "radPickupPay"
+        Me.radPickupPay.Size = New System.Drawing.Size(115, 21)
+        Me.radPickupPay.TabIndex = 14
+        Me.radPickupPay.TabStop = True
+        Me.radPickupPay.Text = "RadioButton7"
+        Me.radPickupPay.UseVisualStyleBackColor = True
         '
-        'GroupBox2
+        'grpPayOption
         '
-        Me.GroupBox2.Controls.Add(Me.RadioButton6)
-        Me.GroupBox2.Controls.Add(Me.RadioButton7)
-        Me.GroupBox2.Location = New System.Drawing.Point(76, 405)
-        Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(200, 100)
-        Me.GroupBox2.TabIndex = 15
-        Me.GroupBox2.TabStop = False
-        Me.GroupBox2.Text = "GroupBox2"
+        Me.grpPayOption.Controls.Add(Me.radPrePay)
+        Me.grpPayOption.Controls.Add(Me.radPickupPay)
+        Me.grpPayOption.Location = New System.Drawing.Point(76, 405)
+        Me.grpPayOption.Name = "grpPayOption"
+        Me.grpPayOption.Size = New System.Drawing.Size(200, 100)
+        Me.grpPayOption.TabIndex = 15
+        Me.grpPayOption.TabStop = False
+        Me.grpPayOption.Text = "GroupBox2"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(460, 388)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 16
+        Me.btnCalculate.Text = "Button1"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(664, 387)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 17
+        Me.btnClear.Text = "Button2"
+        Me.btnClear.UseVisualStyleBackColor = True
         '
         'frmCatering
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(853, 601)
-        Me.Controls.Add(Me.GroupBox2)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.GroupBox1)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.PictureBox1)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.grpPayOption)
+        Me.Controls.Add(Me.lblTotalCost)
+        Me.Controls.Add(Me.grpPlatterOptions)
+        Me.Controls.Add(Me.lblPayment)
+        Me.Controls.Add(Me.picPlatter)
+        Me.Controls.Add(Me.txtPoints)
+        Me.Controls.Add(Me.lblLoyaltyPoints)
+        Me.Controls.Add(Me.lblSubheading)
+        Me.Controls.Add(Me.lblHeading)
         Me.Name = "frmCatering"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Catering"
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.GroupBox1.ResumeLayout(False)
-        Me.GroupBox1.PerformLayout()
-        Me.GroupBox2.ResumeLayout(False)
-        Me.GroupBox2.PerformLayout()
+        CType(Me.picPlatter, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.grpPlatterOptions.ResumeLayout(False)
+        Me.grpPlatterOptions.PerformLayout()
+        Me.grpPayOption.ResumeLayout(False)
+        Me.grpPayOption.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Label3 As Label
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents Label4 As Label
-    Friend WithEvents RadioButton1 As RadioButton
-    Friend WithEvents RadioButton2 As RadioButton
-    Friend WithEvents RadioButton3 As RadioButton
-    Friend WithEvents RadioButton4 As RadioButton
-    Friend WithEvents RadioButton5 As RadioButton
-    Friend WithEvents GroupBox1 As GroupBox
-    Friend WithEvents Label5 As Label
-    Friend WithEvents RadioButton6 As RadioButton
-    Friend WithEvents RadioButton7 As RadioButton
-    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents lblSubheading As Label
+    Friend WithEvents lblLoyaltyPoints As Label
+    Friend WithEvents txtPoints As TextBox
+    Friend WithEvents picPlatter As PictureBox
+    Friend WithEvents lblPayment As Label
+    Friend WithEvents radFruit As RadioButton
+    Friend WithEvents radDeliMeat As RadioButton
+    Friend WithEvents radPita As RadioButton
+    Friend WithEvents radGrill As RadioButton
+    Friend WithEvents radVeggie As RadioButton
+    Friend WithEvents grpPlatterOptions As GroupBox
+    Friend WithEvents lblTotalCost As Label
+    Friend WithEvents radPrePay As RadioButton
+    Friend WithEvents radPickupPay As RadioButton
+    Friend WithEvents grpPayOption As GroupBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
 End Class
