86af9af5ae5ca1671c81475ff9db25d0bef82cff
TaoMaru <jackson_maria1@columbusstate.edu>
2023-03-20 17:10:42 -0400

    Add text and font styling to lbls, btns, rads, and txt areas

--- frmCatering.Designer.vb
+++ frmCatering.Designer.vb
@@ -48,37 +48,42 @@ Partial Class frmCatering
         '
         'lblHeading
         '
-        Me.lblHeading.AutoSize = True
-        Me.lblHeading.Location = New System.Drawing.Point(136, 34)
+        Me.lblHeading.Font = New System.Drawing.Font("Century Schoolbook", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeading.Location = New System.Drawing.Point(81, 9)
         Me.lblHeading.Name = "lblHeading"
-        Me.lblHeading.Size = New System.Drawing.Size(51, 17)
+        Me.lblHeading.Size = New System.Drawing.Size(291, 72)
         Me.lblHeading.TabIndex = 0
-        Me.lblHeading.Text = "Label1"
+        Me.lblHeading.Text = "Catering"
+        Me.lblHeading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'lblSubheading
         '
-        Me.lblSubheading.AutoSize = True
-        Me.lblSubheading.Location = New System.Drawing.Point(139, 81)
+        Me.lblSubheading.Font = New System.Drawing.Font("Century Schoolbook", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSubheading.Location = New System.Drawing.Point(115, 68)
         Me.lblSubheading.Name = "lblSubheading"
-        Me.lblSubheading.Size = New System.Drawing.Size(51, 17)
+        Me.lblSubheading.Size = New System.Drawing.Size(222, 47)
         Me.lblSubheading.TabIndex = 1
-        Me.lblSubheading.Text = "Label2"
+        Me.lblSubheading.Text = "Star Market"
+        Me.lblSubheading.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
         'lblLoyaltyPoints
         '
-        Me.lblLoyaltyPoints.AutoSize = True
-        Me.lblLoyaltyPoints.Location = New System.Drawing.Point(497, 352)
+        Me.lblLoyaltyPoints.Font = New System.Drawing.Font("Century Schoolbook", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLoyaltyPoints.Location = New System.Drawing.Point(451, 329)
         Me.lblLoyaltyPoints.Name = "lblLoyaltyPoints"
-        Me.lblLoyaltyPoints.Size = New System.Drawing.Size(51, 17)
+        Me.lblLoyaltyPoints.Size = New System.Drawing.Size(315, 65)
         Me.lblLoyaltyPoints.TabIndex = 2
-        Me.lblLoyaltyPoints.Text = "Label3"
+        Me.lblLoyaltyPoints.Text = "Enter Loyalty Points:"
+        Me.lblLoyaltyPoints.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'txtPoints
         '
-        Me.txtPoints.Location = New System.Drawing.Point(708, 352)
+        Me.txtPoints.Font = New System.Drawing.Font("Century Schoolbook", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPoints.Location = New System.Drawing.Point(808, 341)
         Me.txtPoints.Name = "txtPoints"
-        Me.txtPoints.Size = New System.Drawing.Size(100, 22)
+        Me.txtPoints.Size = New System.Drawing.Size(79, 40)
         Me.txtPoints.TabIndex = 3
+        Me.txtPoints.Text = "8888"
         '
         'picPlatter
         '
@@ -93,66 +98,72 @@ Partial Class frmCatering
         '
         'lblPayment
         '
-        Me.lblPayment.AutoSize = True
-        Me.lblPayment.Location = New System.Drawing.Point(203, 568)
+        Me.lblPayment.Font = New System.Drawing.Font("Century Schoolbook", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPayment.Location = New System.Drawing.Point(115, 578)
         Me.lblPayment.Name = "lblPayment"
-        Me.lblPayment.Size = New System.Drawing.Size(51, 17)
+        Me.lblPayment.Size = New System.Drawing.Size(148, 43)
         Me.lblPayment.TabIndex = 5
-        Me.lblPayment.Text = "Label4"
+        Me.lblPayment.Text = "Payment:"
+        Me.lblPayment.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         '
         'radFruit
         '
         Me.radFruit.AutoSize = True
-        Me.radFruit.Location = New System.Drawing.Point(78, 45)
+        Me.radFruit.Font = New System.Drawing.Font("Century Schoolbook", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.radFruit.Location = New System.Drawing.Point(37, 31)
         Me.radFruit.Name = "radFruit"
-        Me.radFruit.Size = New System.Drawing.Size(115, 21)
+        Me.radFruit.Size = New System.Drawing.Size(87, 29)
         Me.radFruit.TabIndex = 6
         Me.radFruit.TabStop = True
-        Me.radFruit.Text = "RadioButton1"
+        Me.radFruit.Text = "Fruit"
         Me.radFruit.UseVisualStyleBackColor = True
         '
         'radDeliMeat
         '
         Me.radDeliMeat.AutoSize = True
-        Me.radDeliMeat.Location = New System.Drawing.Point(78, 84)
+        Me.radDeliMeat.Font = New System.Drawing.Font("Century Schoolbook", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.radDeliMeat.Location = New System.Drawing.Point(37, 77)
         Me.radDeliMeat.Name = "radDeliMeat"
-        Me.radDeliMeat.Size = New System.Drawing.Size(115, 21)
+        Me.radDeliMeat.Size = New System.Drawing.Size(133, 29)
         Me.radDeliMeat.TabIndex = 7
         Me.radDeliMeat.TabStop = True
-        Me.radDeliMeat.Text = "RadioButton2"
+        Me.radDeliMeat.Text = "Deli Meat"
         Me.radDeliMeat.UseVisualStyleBackColor = True
         '
         'radPita
         '
         Me.radPita.AutoSize = True
-        Me.radPita.Location = New System.Drawing.Point(78, 129)
+        Me.radPita.Font = New System.Drawing.Font("Century Schoolbook", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.radPita.Location = New System.Drawing.Point(37, 123)
         Me.radPita.Name = "radPita"
-        Me.radPita.Size = New System.Drawing.Size(115, 21)
+        Me.radPita.Size = New System.Drawing.Size(76, 29)
         Me.radPita.TabIndex = 8
         Me.radPita.TabStop = True
-        Me.radPita.Text = "RadioButton3"
+        Me.radPita.Text = "Pita"
         Me.radPita.UseVisualStyleBackColor = True
         '
         'radGrill
         '
         Me.radGrill.AutoSize = True
-        Me.radGrill.Location = New System.Drawing.Point(78, 170)
+        Me.radGrill.Font = New System.Drawing.Font("Century Schoolbook", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.radGrill.Location = New System.Drawing.Point(37, 169)
         Me.radGrill.Name = "radGrill"
-        Me.radGrill.Size = New System.Drawing.Size(115, 21)
+        Me.radGrill.Size = New System.Drawing.Size(81, 29)
         Me.radGrill.TabIndex = 9
         Me.radGrill.TabStop = True
-        Me.radGrill.Text = "RadioButton4"
+        Me.radGrill.Text = "Grill"
         Me.radGrill.UseVisualStyleBackColor = True
         '
         'radVeggie
         '
         Me.radVeggie.AutoSize = True
-        Me.radVeggie.Location = New System.Drawing.Point(78, 198)
+        Me.radVeggie.Font = New System.Drawing.Font("Century Schoolbook", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.radVeggie.Location = New System.Drawing.Point(37, 215)
         Me.radVeggie.Name = "radVeggie"
-        Me.radVeggie.Size = New System.Drawing.Size(115, 21)
+        Me.radVeggie.Size = New System.Drawing.Size(101, 29)
         Me.radVeggie.TabIndex = 10
         Me.radVeggie.TabStop = True
-        Me.radVeggie.Text = "RadioButton5"
+        Me.radVeggie.Text = "Veggie"
         Me.radVeggie.UseVisualStyleBackColor = True
         '
         'grpPlatterOptions
@@ -162,71 +173,79 @@ Partial Class frmCatering
         Me.grpPlatterOptions.Controls.Add(Me.radDeliMeat)
         Me.grpPlatterOptions.Controls.Add(Me.radGrill)
         Me.grpPlatterOptions.Controls.Add(Me.radPita)
-        Me.grpPlatterOptions.Location = New System.Drawing.Point(45, 131)
+        Me.grpPlatterOptions.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.grpPlatterOptions.Location = New System.Drawing.Point(65, 131)
         Me.grpPlatterOptions.Name = "grpPlatterOptions"
         Me.grpPlatterOptions.Size = New System.Drawing.Size(272, 253)
         Me.grpPlatterOptions.TabIndex = 11
         Me.grpPlatterOptions.TabStop = False
-        Me.grpPlatterOptions.Text = "GroupBox1"
+        Me.grpPlatterOptions.Text = "Select Platter"
         '
         'lblTotalCost
         '
-        Me.lblTotalCost.AutoSize = True
-        Me.lblTotalCost.Location = New System.Drawing.Point(484, 568)
+        Me.lblTotalCost.Font = New System.Drawing.Font("Century Schoolbook", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalCost.Location = New System.Drawing.Point(343, 563)
         Me.lblTotalCost.Name = "lblTotalCost"
-        Me.lblTotalCost.Size = New System.Drawing.Size(51, 17)
+        Me.lblTotalCost.Size = New System.Drawing.Size(596, 72)
         Me.lblTotalCost.TabIndex = 12
-        Me.lblTotalCost.Text = "Label5"
+        Me.lblTotalCost.Text = "Your order Option Platter Costs 88888 Using" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Payment Option after Discount of 888" &
+    "8 Loyalty Points"
+        Me.lblTotalCost.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         '
         'radPrePay
         '
         Me.radPrePay.AutoSize = True
-        Me.radPrePay.Location = New System.Drawing.Point(37, 30)
+        Me.radPrePay.Font = New System.Drawing.Font("Century Schoolbook", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.radPrePay.Location = New System.Drawing.Point(37, 31)
         Me.radPrePay.Name = "radPrePay"
-        Me.radPrePay.Size = New System.Drawing.Size(115, 21)
+        Me.radPrePay.Size = New System.Drawing.Size(115, 29)
         Me.radPrePay.TabIndex = 13
         Me.radPrePay.TabStop = True
-        Me.radPrePay.Text = "RadioButton6"
+        Me.radPrePay.Text = "Pre-Pay"
         Me.radPrePay.UseVisualStyleBackColor = True
         '
         'radPickupPay
         '
         Me.radPickupPay.AutoSize = True
-        Me.radPickupPay.Location = New System.Drawing.Point(37, 57)
+        Me.radPickupPay.Font = New System.Drawing.Font("Century Schoolbook", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.radPickupPay.Location = New System.Drawing.Point(37, 76)
         Me.radPickupPay.Name = "radPickupPay"
-        Me.radPickupPay.Size = New System.Drawing.Size(115, 21)
+        Me.radPickupPay.Size = New System.Drawing.Size(178, 29)
         Me.radPickupPay.TabIndex = 14
         Me.radPickupPay.TabStop = True
-        Me.radPickupPay.Text = "RadioButton7"
+        Me.radPickupPay.Text = "Pay at Pickup"
         Me.radPickupPay.UseVisualStyleBackColor = True
         '
         'grpPayOption
         '
         Me.grpPayOption.Controls.Add(Me.radPrePay)
         Me.grpPayOption.Controls.Add(Me.radPickupPay)
-        Me.grpPayOption.Location = New System.Drawing.Point(76, 405)
+        Me.grpPayOption.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.grpPayOption.Location = New System.Drawing.Point(65, 403)
         Me.grpPayOption.Name = "grpPayOption"
-        Me.grpPayOption.Size = New System.Drawing.Size(200, 100)
+        Me.grpPayOption.Size = New System.Drawing.Size(272, 135)
         Me.grpPayOption.TabIndex = 15
         Me.grpPayOption.TabStop = False
-        Me.grpPayOption.Text = "GroupBox2"
+        Me.grpPayOption.Text = "Select Payment"
         '
         'btnCalculate
         '
-        Me.btnCalculate.Location = New System.Drawing.Point(460, 434)
+        Me.btnCalculate.Font = New System.Drawing.Font("Century Schoolbook", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(476, 434)
         Me.btnCalculate.Name = "btnCalculate"
-        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.Size = New System.Drawing.Size(198, 49)
         Me.btnCalculate.TabIndex = 16
-        Me.btnCalculate.Text = "Button1"
+        Me.btnCalculate.Text = "Calculate"
         Me.btnCalculate.UseVisualStyleBackColor = True
         '
         'btnClear
         '
-        Me.btnClear.Location = New System.Drawing.Point(689, 435)
+        Me.btnClear.Font = New System.Drawing.Font("Century Schoolbook", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(730, 434)
         Me.btnClear.Name = "btnClear"
-        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.Size = New System.Drawing.Size(198, 49)
         Me.btnClear.TabIndex = 17
-        Me.btnClear.Text = "Button2"
+        Me.btnClear.Text = "Clear"
         Me.btnClear.UseVisualStyleBackColor = True
         '
         'frmCatering
